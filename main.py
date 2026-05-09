3ec2f27aa6c23e94a00e969348c2a0fba69fa800
madhukamath2000-lang <madhu.kamath2000@gmail.com>
2026-05-09 18:02:13 +1000

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ app = FastAPI()
 
 app.add_middleware(
 CORSMiddleware,
-allow_origins=[”*”],
-allow_methods=[”*”],
-allow_headers=[”*”],
+allow_origins=["*"],
+allow_methods=["*"],
+allow_headers=["*"],
 )
 
 TOKEN = os.environ.get(“UPSTOX_TOKEN”, “eyJ0eXAiOiJKV1QiLCJrZXlfaWQiOiJza192MS4wIiwiYWxnIjoiSFMyNTYifQ.eyJzdWIiOiIyMjEzMzgiLCJqdGkiOiI2OWZhODdmYTA3YzlmYTFmZjhkYTI5NzMiLCJpc011bHRpQ2xpZW50IjpmYWxzZSwiaXNQbHVzUGxhbiI6dHJ1ZSwiaXNFeHRlbmRlZCI6dHJ1ZSwiaWF0IjoxNzc4MDI2NDkwLCJpc3MiOiJ1ZGFwaS1nYXRld2F5LXNlcnZpY2UiLCJleHAiOjE4MDk2NDA4MDB9.Hexx8mI8Hj8HTKcYd8-zzKnXyIY25X6-X8wo56EQyvk”)
